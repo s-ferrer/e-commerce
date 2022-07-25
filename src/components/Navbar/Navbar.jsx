60ad8b8d8e632de9db8bc6ea731193733e692b12
s-ferrer <saraferrer@gmail.com>
2022-07-25 18:05:07 +0200

    Create and Style Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+import {
+  AppBar,
+  Toolbar,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+  IconButton,
+} from "@material-ui/core";
+
+import { ShoppingCart } from "@material-ui/icons";
+import logo from "../../assets/commerce.png";
+import useStyles from "./styles";
+
+const Navbar = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography variant="h6" className={classes.tittle} color="inherit">
+            <img
+              src={logo}
+              alt="E-commerce"
+              height="25px"
+              className={classes.image}
+            />
+            E-Commerce
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label="Show cart items" color="inherit">
+              <Badge badgeContent={2} color="secundary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Navbar;
