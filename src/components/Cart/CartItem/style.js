c75a6be1adfe6740f97a8d8c68bf4b2c9ac5fb69
s-ferrer <saraferrer@gmail.com>
2022-07-27 16:57:28 +0200

    Create and Style CartItem components

---
+++ src/components/Cart/CartItem/style.js
@@ -0,0 +1,19 @@
+import styles from "../styles";
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles(() => ({
+  media: {
+    height: 260,
+  },
+  cardContent: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  cardActions: {
+    justifyContent: "space-between",
+  },
+  buttons: {
+    display: "flex",
+    alignItems: "center",
+  },
+}));
