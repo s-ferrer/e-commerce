70fd44b322837933f8f86e3cc32edbb6abd6b4a5
s-ferrer <saraferrer@gmail.com>
2022-07-27 17:28:39 +0200

    Create CartItem Layout

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -8,10 +8,39 @@ import {
   CardMedia,
 } from "@material-ui/core";
 
-import useStyles from "./styles";
+import useStyles from "./style";
 
-const CartItem = () => {
-  return <div>CartItem</div>;
+const CartItem = ({ item }) => {
+  const classes = useStyles();
+  return (
+    <Card>
+      <CardMedia
+        className={classes.media}
+        image={CartItem.image?.url}
+        alt={item.name}
+        style={{ minHeight: 300 }}
+      />
+      <CardContent className={classes.CardContent}>
+        <Typography variant="h4">{item.name}</Typography>
+        <Typography variant="h5">
+          {item.line_total.formatted_with_symbol}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <div className={classes.buttons}>
+          <Button type="button" size="small">
+            -
+          </Button>
+          <Typography>{item.quantity}</Typography>
+          <Button type="button" size="small">
+            +
+          </Button>
+        </div>
+        <Button varient="contained" type="button" color="secondary">
+          Remove
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
-
 export default CartItem;
