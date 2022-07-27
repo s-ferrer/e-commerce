7896ea3be3c68ffbef6c02ddd01d07fa86275d44
s-ferrer <saraferrer@gmail.com>
2022-07-27 16:57:05 +0200

    Style Cart components

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Container, Typography, Button, Grid } from "@material-ui/core";
 
+import useStyles from "./styles";
+
 const Cart = ({ cart }) => {
-  const isEmpty = !cart.line_items.length;
+  const classes = useStyles();
 
   const EmptyCart = () => (
     <Typography variant="subtitle1">
@@ -47,13 +49,14 @@ const Cart = ({ cart }) => {
     </>
   );
 
+  if (!cart.line_items) return "Loading...";
   return (
     <Container>
       <div className={classes.toolbar} />
       <Typography className={classes.title} variant="h3">
         Your Shopping Cart
       </Typography>
-      {isEmpty ? <EmptyCart /> : <FilledCart />}
+      {!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
     </Container>
   );
 };
