cf1ea52fed1bbae76a01829092da0f6a69f65075
s-ferrer <saraferrer@gmail.com>
2022-08-02 12:05:27 +0200

    Add and Style Checkout Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { commerce } from "./lib/commerce";
-import { Products, Navbar, Cart } from "./components";
+import { Products, Navbar, Cart, Checkout } from "./components";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -72,6 +72,7 @@ const App = () => {
               />
             }
           />
+          <Route exact path="/checkout" element={<Checkout cart={cart} />} />
         </Routes>
       </div>
     </Router>
