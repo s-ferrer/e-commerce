a103f19a21d14fe3eab60a41b382643e82b1b549
s-ferrer <saraferrer@gmail.com>
2022-08-09 16:05:27 +0200

    Add Grid, value and onChange

--- src/components/CheckoutForm/CustomTextField.jsx
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -2,18 +2,26 @@ import React from "react";
 import { TextField, Grid } from "@material-ui/core";
 import { useFormContext, Controller } from "react-hook-form";
 
-const FormInput = ({ name, label, required }) => {
+const FormInput = ({ name, label, value, onChange }) => {
   const { control } = useFormContext();
-  /*const isError = false;*/
 
   return (
-    <>
+    <Grid item xs={12} sm={6}>
       <Controller
         control={control}
+        defaultValue=""
         name={name}
-        render={({ field }) => <TextField fullWidth label={label} required />}
+        render={({ field }) => (
+          <TextField
+            fullWidth
+            label={label}
+            value={value}
+            onChange={onChange}
+            required
+          />
+        )}
       />
-    </>
+    </Grid>
   );
 };
 export default FormInput;
