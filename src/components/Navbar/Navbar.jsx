3596203414e09525456e5cc5e69616c7b5c6f1b2
s-ferrer <saraferrer@gmail.com>
2022-08-09 16:07:49 +0200

    Updated and finish Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
   AppBar,
@@ -12,12 +12,48 @@ import {
 
 import { ShoppingCart } from "@material-ui/icons";
 import { Link, useLocation } from "react-router-dom";
+
 import logo from "../../assets/icon.png";
 import useStyles from "./styles";
 
-const Navbar = ({ totalItems }) => {
+const PrimarySearchAppBar = ({ totalItems }) => {
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
   const classes = useStyles();
   const location = useLocation();
+
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+  const handleMobileMenuClose = () => setMobileMoreAnchorEl(null);
+  const mobileMenuId = "primary-search-account-menu-mobile";
+
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton
+          component={Link}
+          to="/cart"
+          aria-label="Show cart items"
+          color="inherit"
+        >
+          <Badge
+            badgeContent={totalItems}
+            color="secondary"
+            overlap="rectangular"
+          >
+            <ShoppingCart />
+          </Badge>
+        </IconButton>
+        <p>Cart</p>
+      </MenuItem>
+    </Menu>
+  );
 
   return (
     <>
@@ -59,8 +95,9 @@ const Navbar = ({ totalItems }) => {
           )}
         </Toolbar>
       </AppBar>
+      {renderMobileMenu}
     </>
   );
 };
 
-export default Navbar;
+export default PrimarySearchAppBar;
