c01bbbd1b6cdf47c1facad0a2e9d99391647d527
s-ferrer <saraferrer@gmail.com>
2022-08-09 16:02:46 +0200

    Add handleCaptureCheckout, set order and error message

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const App = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState({});
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState("");
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
@@ -41,6 +43,19 @@ const App = () => {
     setCart(newCart);
   };
 
+  const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+    try {
+      const incomingOrder = await commerce.checkout.capture(
+        checkoutTokenId,
+        newOrder
+      );
+      setOrder(incomingOrder);
+      refreshCart();
+    } catch (error) {
+      setErrorMessage(error.data.error.errorMessage);
+    }
+  };
+
   useEffect(() => {
     fetchProducts();
     fetchCart();
@@ -70,7 +85,18 @@ const App = () => {
               />
             }
           />
-          <Route exact path="/checkout" element={<Checkout cart={cart} />} />
+          <Route
+            exact
+            path="/checkout"
+            element={
+              <Checkout
+                cart={cart}
+                order={order}
+                onCaptureCheckout={handleCaptureCheckout}
+                error={errorMessage}
+              />
+            }
+          />
         </Routes>
       </div>
     </Router>
